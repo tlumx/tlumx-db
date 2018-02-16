0a8e8f2a77aaf4237220bd81d870e84a64827fe2
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-02-17 00:30:38 +0200

    Add PHPUnit bootstrap configuration files

---
+++ tests/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Tlumx (https://tlumx.github.io/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/tlumx-db
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/tlumx-db/blob/master/LICENSE  (MIT License)
+ */
