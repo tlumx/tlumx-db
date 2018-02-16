b763526e5a921a12ba2065edf08a75c95e088668
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-02-16 23:47:57 +0200

    Create main db profiler class/file

---
+++ src/DbProfiler.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Tlumx (https://tlumx.github.io/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/tlumx-db
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/tlumx-db/blob/master/LICENSE  (MIT License)
+ */
+namespace Tlumx\Db;
+
+/**
+ * Db profiler class.
+ */
+class DbProfiler
+{
+
+}
