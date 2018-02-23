1a21dc6a6f8d1d97286f6d0cad3095714e32ad95
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-02-23 22:37:00 +0200

    .

---
+++ tests/Db/DbExceptionTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tlumx (https://tlumx.github.io/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/tlumx-db
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/tlumx-db/blob/master/LICENSE  (MIT License)
+ */
+namespace Tlumx\Tests\Db;
+
+use Tlumx\Db\Exception\DbException;
+use Tlumx\Tests\Db\Foo;
+
+class DbExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplements()
+    {
+        $this->assertInstanceOf(\Exception::class, new DbException);
+    }
+}
